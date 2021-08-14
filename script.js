606141acc416fc439a77c9b062aefdf3f24a593d
Amanda Tolfo Porto <amanda.tporto@gmail.com>
2021-08-14 07:09:45 -0300

    front-end + javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.querySelector(".hamburger").addEventListener("click", () => 
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+var btnSignup = document.querySelector("signUp");
+var btnSignin = document.querySelector("signIn");
+
+var body = document.querySelector("body");
+
+btnSignup addEventListener("click", function () {
+    body.className = "signUp-js";
+});
+
+btnSignin addEventListener("click", function () {
+    body.className = "signIn-js";
+});
